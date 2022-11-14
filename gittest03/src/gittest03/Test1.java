417e2094b8098d1a7a505bd67f2c41d9bc650d5a
user <user@host.docker.internal>
2022-11-14 17:54:50 +0900

    2

--- gittest03/src/gittest03/Test1.java
+++ gittest03/src/gittest03/Test1.java
@@ -1,7 +1,13 @@
 package gittest03;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println(1);
+		
+		System.out.println(new SimpleDateFormat("yyyy.MM.dd").format(new Date()));
+		// 날짜 추가
 	}
 }
